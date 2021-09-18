3e916a38f20722e07ebf72f4a9179d0b6ed9c46e
Sahil Saini <46193454+sahil137@users.noreply.github.com>
2021-09-18 17:43:36 +0530

    add setAlarm function

--- script.js
+++ script.js
@@ -1,8 +1,44 @@
 // Selectors
 
+const alarmInput = document.getElementById("alarm-time");
+const setAlarmButton = document.getElementById("set-alarm-button");
+
 // Event Listeners
 
+setAlarmButton.addEventListener("click", setAlarm);
+
 // Functions
+
+function setAlarm(event) {
+  event.preventDefault();
+  let alarmInputAsNumber = alarmInput.valueAsNumber;
+  console.log(alarmInputAsNumber);
+  if (isNaN(alarmInputAsNumber)) {
+    alert("Please Enter a valid time");
+    return;
+  }
+  const currDate = new Date();
+
+  const hours = currDate.getHours();
+  const minutes = currDate.getMinutes();
+  const seconds = currDate.getSeconds();
+
+  const hoursInMilliseconds = 3600000 * hours;
+  const minutesInMilliseconds = 60000 * minutes;
+  const secondsInMilliseconds = 1000 * seconds;
+
+  const totalTimeInMilliseconds =
+    hoursInMilliseconds + minutesInMilliseconds + secondsInMilliseconds;
+
+  const differenceInTime = alarmInputAsNumber - totalTimeInMilliseconds;
+
+  const id = setTimeout(initializeAlarm, differenceInTime);
+}
+
+function initializeAlarm() {
+  alert("The Alarm is going off");
+}
+
 function updateTime() {
   let date = new Date();
 
